8fa77b2f21018ffde7ada1a8187e7ca2986cfb55
AndresAjalla <141284508+AndresAjalla@users.noreply.github.com>
2023-10-18 23:13:47 -0300

    Add files via upload

---
+++ ejercicio 3s.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int matriz[5][5],fila=0,col=0;
+
+	for (fila=0;fila<5;fila++) {
+		for (col=0;col<5;col++){
+			printf ("\n De la fila #%d, ingrese valor #%d: ",fila+1,col+1);
+			scanf ("%d",&matriz [fila][col]);
+			
+		}
+	}
+	printf("Diagonal principal:\n");
+	for (fila=0;fila<5;fila++) {
+		for (col=0;col<5;col++){
+			if (fila == col) {
+				printf("%d\t", matriz[fila][col]);
+			} 
+			else 
+			{
+				printf("\t");
+			}
+		}
+		printf("\n");
+	}
+	
+	printf("Triángulo superior:\n");
+	for (fila=0;fila<5;fila++) {
+		for (col=0;col<5;col++){
+			if (fila <= col) {
+				printf("%d\t", matriz[fila][col]);
+			} else {
+				printf("\t");
+			}
+		}
+		printf("\n");
+	}
+	
+	printf("Triángulo inferior:\n");
+	for (fila=0;fila<5;fila++) {
+		for (col=0;col<5;col++){
+			if (fila >= col) {
+				printf("%d\t", matriz[fila][col]);
+			} else {
+				printf("\t");
+			}
+		}
+		printf("\n");
+	}
+	
+	system("pause");
+	return 0;
+}
